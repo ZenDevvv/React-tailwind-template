8c1060eeeff53cd54c3a74b6a44d1e5e5ddb2f19
ZenDev <ozenandreiii@gmail.com>
2024-11-02 13:21:52 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import{ useState } from "react";
+import styles from "./App.module.css";
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl text-center text-blue-300">Helo world</h1>
+    </>
+  );
+}
+
+export default App;
